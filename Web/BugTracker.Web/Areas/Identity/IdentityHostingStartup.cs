3d420c211c990241bc834b65458c87f9f779fcd8
m.pavlov <57910640+MihaelPavlov@users.noreply.github.com>
2022-01-24 23:50:46 +0200

    Scaffolding and update the design of Identity pages

---
+++ Web/BugTracker.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BugTracker.Data;
+using BugTracker.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BugTracker.Web.Areas.Identity.IdentityHostingStartup))]
+namespace BugTracker.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
